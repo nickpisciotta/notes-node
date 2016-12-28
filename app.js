f92a0ae8d1fc508cebfb0875aa76aa96d3cfd379
Nick Pisciotta <npisciotta@gmail.com>
2016-12-28 11:39:02 -0700

    Add support for command line flags Use Yargs module to parse command line arguments

--- app.js
+++ app.js
@@ -2,5 +2,22 @@ console.log("Starting app.js");
 
 const fs = require("fs"); 
 const _ = require("lodash"); 
+const yargs = require("yargs"); 
 
 const notes = require ("./notes.js"); 
+
+const argv = yargs.argv; 
+var command = process.argv[2]; 
+
+if (command === "add") {
+  notes.addNote(argv.title, argv.body); 
+} else if (command === "list") {
+  console.log("Listing all notes"); 
+} else if (command === "read") {
+  console.log("Reading note"); 
+} else if (command === "remove") {
+  console.log("Deleting Note"); 
+} else {
+  console.log("Command not recognized"); 
+}
+
