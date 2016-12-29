f311eeca1550ded2bdd0a3b85e2bdc82336f11dc
Nick Pisciotta <npisciotta@gmail.com>
2016-12-28 17:23:06 -0700

    Add read command functionality

--- app.js
+++ app.js
@@ -10,16 +10,19 @@ var command = argv._[0];
 if (command === "add") {
   var note = notes.addNote(argv.title, argv.body); 
   if (note) {
-    console.log('---'); 
-    console.log(`Title: ${note.title}`); 
-    console.log(`Body: ${note.body}`)
+    notes.logNote(note); 
   } else {
     console.log("A note with that title has already been added");
   }
 } else if (command === "list") {
   notes.getAll(); 
 } else if (command === "read") {
-  notes.getNote(argv.title); 
+  var note = notes.getNote(argv.title); 
+  if (note) {
+    notes.logNote(note); 
+  } else {
+    console.log("Note not found"); 
+  }
 } else if (command === "remove") {
   var noteRemoved = notes.removeNote(argv.title); 
   var message = noteRemoved ? "Note was removed" : "Note not found";
--- notes.js
+++ notes.js
@@ -33,20 +33,29 @@ var getAll = () => {
 }; 
 
 var getNote = (title) => {
-  console.log("Reading Note:", title); 
+  var notes = fetchNotes(); 
+  var filteredNote = notes.filter((note) => note.title === title); 
+  return filteredNote[0]; 
 }
 
 var removeNote = (title) => {
   var notes = fetchNotes(); 
   var filteredNotes = notes.filter((note) => note.title !== title);
   saveNotes(filteredNotes); 
-  
+
   return notes.length !== filteredNotes.length; 
+}
+
+var logNote = (note) => {
+  console.log('---'); 
+  console.log(`Title: ${note.title}`); 
+  console.log(`Body: ${note.body}`)  
 }
 
 module.exports = {
   addNote, 
   getAll, 
   getNote, 
-  removeNote
+  removeNote, 
+  logNote
 }; 
